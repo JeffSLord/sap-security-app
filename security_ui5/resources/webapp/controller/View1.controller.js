f403430c84e7904bda568f18c23f41a51b77ac45
i849438 <jeffrey.lord@sap.com>
2019-10-18 16:36:17 +0000

    admin working. buttons added.

--- security_ui5/resources/webapp/controller/View1.controller.js
+++ security_ui5/resources/webapp/controller/View1.controller.js
@@ -19,9 +19,9 @@ sap.ui.define([
 		},
 		onBeforeRendering: function () {
 			var iconBar = this.getView().byId("iconBar");
-			var adminFragment = sap.ui.xmlfragment("ui5_ns.security_ui5.view.AdminTab");
-			var res_data = $.ajax({
-				url: '/api/scopes/edit',
+			var adminFragment = sap.ui.xmlfragment("ui5_ns.security_ui5.view.AdminTab", this);
+			$.ajax({
+				url: '/api/scopes/read',
 				timeout: 360000,
 				headers: {
 					'Accept': 'application/json'
@@ -44,17 +44,68 @@ sap.ui.define([
 					// iconBar.destroyItems();
 				}
 			});
-			// var form = this.getView().byId(this.createId("formId"));
-			// var MAX = 5;
-			// for (var i = 0; i < MAX; i++) {
-			// 	form.addContent(new sap.m.Label({
-			// 		text: "label"
-			// 	}));
-			// 	form.addContent(new sap.m.Text({
-			// 		text: "text"
-			// 	}));
-			// }
-			// console.log(form.getContent());
+		},
+		buttonRead: function () {
+			$.ajax({
+				url: '/api/scopes/read',
+				timeout: 360000,
+				headers: {
+					'Accept': 'application/json'
+				},
+				'Accept': 'application/json',
+				type: 'get',
+				contentType: false,
+				processData: false,
+				success: (res) => {
+					let body = res.responseJSON;
+					console.log(body);
+					this.showToast('VALID');
+				},
+				error: (res) => {
+					let body = res.responseJSON;
+					console.log(body);
+					this.showToast('INVALID');
+				}
+			});
+		},
+		buttonEdit: function () {
+			$.ajax({
+				url: '/api/scopes/edit',
+				timeout: 360000,
+				headers: {
+					'Accept': 'application/json'
+				},
+				'Accept': 'application/json',
+				type: 'get',
+				contentType: false,
+				processData: false,
+				success: (res) => {
+					let body = res.responseJSON;
+					console.log(body);
+					this.showToast('VALID');
+				},
+				error: (res) => {
+					let body = res.responseJSON;
+					console.log(body);
+					this.showToast('INVALID');
+				}
+			});
+		},
+		showToast: function (message, duration = 3000) {
+			sap.m.MessageToast.show(message, {
+				duration: duration, // default
+				width: "15em", // default
+				my: "center bottom", // default
+				at: "center bottom", // default
+				of: window, // default
+				offset: "0 0", // default
+				collision: "fit fit", // default
+				onClose: null, // default
+				autoClose: true, // default
+				animationTimingFunction: "ease", // default
+				animationDuration: 1000, // default
+				closeOnBrowserNavigation: true // default
+			});
 		}
 	});
 });

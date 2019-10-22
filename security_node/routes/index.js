3b943a298486dda61bf265bbb8381d7dd8c433c6
i849438 <jeffrey.lord@sap.com>
2019-10-22 17:11:20 +0000

    added select route

--- security_node/routes/index.js
+++ security_node/routes/index.js
@@ -5,6 +5,7 @@ module.exports = (app, server) => {
 	app.use('/api/scopes', require("./scopes.route"));
 	app.use('/api/attributes', require("./attributes.route"));
 	app.use('/api/user', require('./user.route'));
+	app.use('/api/select', require('./select.route'));
 	app.use('/api/auth', (req, res)=>{
 		res.send(req.authInfo);
 	});
